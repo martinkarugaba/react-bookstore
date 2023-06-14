8f8705feb13ad76372a70983ade160a9f2e6a72c
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-14 13:18:10 +0300

    Fix linter errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   booksArr: [],
 };
 
 export const booksSlice = createSlice({
-  name: "books",
+  name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      const { id, title, author, category } = action.payload;
+      const {
+        id, title, author, category,
+      } = action.payload;
       const book = {
         id,
         title,
         author,
-        category
+        category,
       };
       state.booksArr.push(book);
     },
     removeBook: (state, action) => {
       const bookId = action.payload;
       state.booksArr = state.booksArr.filter(
-        (book) => book.id !== bookId
+        (book) => book.id !== bookId,
       );
     },
   },
